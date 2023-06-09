5c312b99b5526917cfad17d306ef582cf58e6f02
Carolin Bittrich <bittrichcarolin@gmail.com>
2023-06-09 12:04:48 -0500

    8_commit

---
+++ 01web_fundam/03JS/01evento_onclick/script.js
@@ -0,0 +1,11 @@
+// CHANGE TEXT OF LOGIN BUTTON TO LOGOUT WHEN CLICKED
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+// REMOVE BUTTON OF DEFINITION WHEN CLICKED
+function hide(element) {
+    element.remove();
+}
+
+// ADD ALERT IN LIKE BUTTON WHEN CLICKED => directo en el HTML
